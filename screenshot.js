981a26fe9b3c28c5aba9756def0ae1206912e13a
Bob Lauer <rlauer@gmail.com>
2013-11-19 20:37:03 -0600

    Simple screen scraper.  Pretty straight-forward

---
+++ screenshot.js
@@ -0,0 +1,44 @@
+var http = require('http')
+  , parse = require('url').parse
+  ;
+
+function getHTMLFromUrl(url, cb) {
+  url = parse(url);
+
+  var options = {
+    host: url.hostname,
+    post: url.port,
+    path: url.pathname,
+  };
+
+  http.get(options, function(res) {
+    var html = '';
+    res.setEncoding('utf-8');
+
+    res.on('data', function(res) {
+      html += res;
+    });
+
+    res.on('end', function() {
+      cb(getImage(html) || getVideo(html));
+    });
+  });
+}
+
+function getImage(html) {
+  var matches = /<img class=\"embeddedObject\" src=(.*\.(?:png|git|jpg|jpeg))/g.exec(html);
+  if (matches && matches[1]) {
+    return '<img src=' + matches[1] + ' />';
+  }
+}
+
+function getVideo(html) {
+  var matches = /<object[\s\S]*object>/g.exec(html);
+  if (matches && matches[0]) {
+    return matches[0];
+  }
+}
+
+module.exports = {
+  get: getHTMLFromUrl
+};
