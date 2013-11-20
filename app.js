7a38f513ad8de57c7e270446706f083a599bda61
boblauer <rlauer@gmail.com>
2013-11-19 22:24:03 -0600

    CORS enabled

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res) {
   var url = decodeURI(req.query.url);
   console.log('url', url);
